9f2e9b856e4c6b865beeb2ebd8ff42734916fe59
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-04-15 23:21:25 +0200

    task 6

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
+class City(BaseModel, Base):
+    """ Rep. a city table im mysql db and inherits from SQLAlchemy
+    Base class"""
     state_id = ""
     name = ""
+    __tablename__= "cities"
+    name = Column(String(128), nullable= False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable = False)
+    
+    
+    
--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models import City, storage
 
+class State(BaseModel, Base):
+    """ Rep. a state table in mysql db and inherits from SQLAlchemy
+    Base class """
+    __tablename__= "states"
+    name = Column(String(128), nullable= False)
+    cities = relationship('City', cascade= 'delete', back_ref='state')
+    if getenv('HBNB_TYPE_STORAGE' != 'db'):
+        @property
+        def cities(self):
+            city_list = []
+            for city in (storage.all(City).values):
+                if (self.id == city.id):
+                    city_list.append(city)
+            return city_list    
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+            
+    
