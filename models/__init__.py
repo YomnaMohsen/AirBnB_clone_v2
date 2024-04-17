1d7d996570aefb451538bb28bb85c79e1cce677b
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-04-17 18:14:42 +0200

    fix

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,12 @@
 """This module instantiates an object of class FileStorage
 and DBStorage depends on env var"""
 from models.engine.file_storage import FileStorage
-#from models.engine.db_storage import DBStorage
-#from os import getenv
+#  from models.engine.db_storage import DBStorage
+# from os import getenv
 
-#if (getenv('HBNB_TYPE_STORAGE') == 'db'):
-    #storage = DBStorage()
+# if (getenv('HBNB_TYPE_STORAGE') == 'db'):
+# storage = DBStorage()
 
-#if (getenv('HBNB_TYPE_STORAGE') == 'file'):
+# if (getenv('HBNB_TYPE_STORAGE') == 'file'):
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            
+
         elif id not in kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -34,7 +34,7 @@ class BaseModel:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-            self.__dict__.update(kwargs)  
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
