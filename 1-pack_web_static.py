f9b3b46329d283ece94ef60b9eab2139fe5e8807
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-07 23:17:38 +0300

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,5 +2,19 @@
 """a Fabric script that generates a .tgz archive 
     from the contents of the web_static folder"""
     
-def  do_pack():
-    pass    
+
+from fabric.api import local
+from datetime import datetime
+    
+def do_pack():
+    """generates .tgz file"""
+    try:
+        local('mkdir -p versions')
+        dt = datetime.utcnow()
+        f_name = "web_static_{}{}{}{}{}{}.tgz".format(
+            dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+        local("tar cf versions/{} web_static".format(f_name))
+        return "versions/"        
+    except Exception:
+        return None   
+      
