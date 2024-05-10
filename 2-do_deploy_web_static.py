ea7c87597bff4c76e73d7707d169e1c4ef54de7f
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-10 23:18:46 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,13 +12,13 @@ env.hosts = ['100.25.145.224', '18.209.225.166']
 
 def do_deploy(archive_path):
     """deploy files on server"""
-    if os.path.isdir(archive_path):
+    if not os.path.isdir(archive_path):
         return False
     file_ext = archive_path.split('/')[-1]
     file_name = file_ext.split('.')[0]
     try:
-        put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}/".format(file_name))
+        put(archive_path, '/tmp/{}'.format(file_ext))
+        run('mkdir -p /data/web_static/releases/{}'.format(file_name))
         run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
             .format(file_ext, file_name))
         run('rm -r /tmp/{}'.format(file_ext))
@@ -29,6 +29,8 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
             .format(file_name))
+        print("done")
         return True
-    except Exception:
+    except Exception as e:
+        print(e)
         return False
