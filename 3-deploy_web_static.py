7474da54c7d176fc703d2013751632e78b190026
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-11 18:41:44 +0300

    t 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,6 @@ from fabric.api import local, env, run, put
 import os.path
 from datetime import datetime
 
-env.hosts = ['100.25.145.224', '18.209.225.166']
 
 
 def do_pack():
@@ -25,27 +24,29 @@ def do_pack():
 
 def do_deploy(archive_path):
     """deploy files on server"""
+    env.hosts = ['100.25.145.224', '18.209.225.166']
     if not os.path.isfile(archive_path):
-        print("deploy file false")
         return False
-    file_ext = archive_path.split('/')[-1]
-    file_name = file_ext.split('.')[0]
     try:
-        put(archive_path, '/tmp/{}'.format(file_ext))
-        run('mkdir -p /data/web_static/releases/{}'.format(file_name))
-        run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(file_ext, file_name))
-        run('rm -r /tmp/{}'.format(file_ext))
-        run('mv /data/web_static/releases/{}/web_static/*  '
-            '/data/web_static/releases/{}/'.format(file_name, file_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.
-            format(file_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(file_name))
-        print("deploy done")
-        return True
-    except Exception as e:
+        for host in env.hosts:
+            env.host_string = host
+            file_ext = archive_path.split('/')[-1]
+            file_name = file_ext.split('.')[0]
+            put(archive_path, '/tmp/{}'.format(file_ext))
+            run('mkdir -p /data/web_static/releases/{}'.format(file_name))
+            run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
+                .format(file_ext, file_name))
+            run('rm -r /tmp/{}'.format(file_ext))
+            run('mv /data/web_static/releases/{}/web_static/*  '
+                '/data/web_static/releases/{}/'.format(file_name, file_name))
+            run('rm -rf /data/web_static/releases/{}/web_static'.
+                format(file_name))
+            run('rm -rf /data/web_static/current')
+            run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+                .format(file_name))
+            print("deploy done")
+            return True
+    except Exception:
         print("exception")
         return False
 
@@ -55,5 +56,4 @@ def deploy():
     arch_path = do_pack()
     if (arch_path is None):
         return False
-    else:
-        return do_deploy(arch_path)
+    return do_deploy(arch_path)
