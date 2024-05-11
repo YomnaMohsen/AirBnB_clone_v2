81be2afdc865a6df2b7800a15c0c81e119b45697
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-11 18:54:17 +0300

    t 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -44,10 +44,8 @@ def do_deploy(archive_path):
             run('rm -rf /data/web_static/current')
             run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
                 .format(file_name))
-            print("deploy done")
-            return True
+        return True
     except Exception:
-        print("exception")
         return False
 
 
