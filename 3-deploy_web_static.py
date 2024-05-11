aa967f3184a89b31c2b35663833a717b5087a4a1
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-11 19:26:53 +0300

    t 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,11 +3,13 @@
 using the function deploy based on do_deploy and do_pack"""
 
 
-from fabric.api import local, env, run, put
+from fabric.api import local, env, run, put, task
 import os.path
 from datetime import datetime
 
+env.hosts = ['100.25.145.224', '18.209.225.166']
 
+@task
 def do_pack():
     """generates .tgz file"""
     try:
@@ -20,34 +22,31 @@ def do_pack():
     except Exception:
         return None
 
-
+@task
 def do_deploy(archive_path):
     """deploy files on server"""
-    env.hosts = ['100.25.145.224', '18.209.225.166']
     if not os.path.isfile(archive_path):
         return False
     try:
-        for host in env.hosts:
-            env.host_string = host
-            file_ext = archive_path.split('/')[-1]
-            file_name = file_ext.split('.')[0]
-            put(archive_path, '/tmp/{}'.format(file_ext))
-            run('mkdir -p /data/web_static/releases/{}'.format(file_name))
-            run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
-                .format(file_ext, file_name))
-            run('rm -r /tmp/{}'.format(file_ext))
-            run('mv /data/web_static/releases/{}/web_static/*  '
-                '/data/web_static/releases/{}/'.format(file_name, file_name))
-            run('rm -rf /data/web_static/releases/{}/web_static'.
-                format(file_name))
-            run('rm -rf /data/web_static/current')
-            run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-                .format(file_name))
+        file_ext = archive_path.split('/')[-1]
+        file_name = file_ext.split('.')[0]
+        put(archive_path, '/tmp/{}'.format(file_ext))
+        run('mkdir -p /data/web_static/releases/{}'.format(file_name))
+        run('tar -zxf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(file_ext, file_name))
+        run('rm -r /tmp/{}'.format(file_ext))
+        run('mv /data/web_static/releases/{}/web_static/*  '
+            '/data/web_static/releases/{}/'.format(file_name, file_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'.
+            format(file_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(file_name))
         return True
     except Exception:
         return False
 
-
+@task
 def deploy():
     """dist archives to web servers"""
     arch_path = do_pack()
