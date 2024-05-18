45700189baebe5cbb4fb1073a76e36487e311fad
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-18 18:07:39 +0300

    fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Flask Module that display many pages /"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,11 +7,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_bnb():
+    """view fn to display string at root page"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """view fn to display string at /hbnb url"""
     return "HBNB"
 
 
