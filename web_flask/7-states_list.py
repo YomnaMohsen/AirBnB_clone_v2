f646d275f3980011492962e32b1ad0cad2279763
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-19 22:42:18 +0300

    t 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Flask Module that display many pages /"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states(n):
+    """view fn to States in an HTML page"""
+    all_states = storage.all(State).values()
+    sorted_list = sorted(all_states, key=lambda x: x.name)
+    return render_template('7-states_list.html', sort_list=sorted_list)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """close current session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
