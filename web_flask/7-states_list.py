cba26c1a3ce610e54bdddc849930f278a08d55e0
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-20 13:30:09 +0300

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def display_states(n):
+def display_states():
     """view fn to States in an HTML page"""
     all_states = storage.all(State).values()
     sorted_list = sorted(all_states, key=lambda x: x.name)
