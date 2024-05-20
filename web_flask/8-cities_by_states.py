01709dfb254189455c5be6a846c79795080d1a18
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-20 14:38:17 +0300

    t 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,18 +3,19 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
+from models.city import City
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def display_states():
     """view fn to States in an HTML page"""
     all_states = storage.all(State).values()
     state_list = sorted(all_states, key=lambda x: x.name)
-    return render_template('7-states_list.html', sort_list=state_list)
-   
+    for s in state_list:
+        s.cities.sort(key=lambda x: x.name)      
+    return render_template('8-cities_by_states.html', sort_list=state_list)
 
 
 @app.teardown_appcontext
