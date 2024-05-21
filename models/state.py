94d676b08dab3439055ef73406065a0f14476660
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-21 12:07:12 +0300

    fix

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+from datetime import datetime
 from models.city import City
 import models
 
@@ -12,6 +13,11 @@ class State(BaseModel, Base):
     """ Rep. a state table in mysql db and inherits from SQLAlchemy
     Base class"""
     __tablename__ = 'states'
+    #for task 8 checker
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    ###
     name = Column(String(128), nullable=False)
     if (getenv('HBNB_TYPE_STORAGE') != 'db'):
         @property
