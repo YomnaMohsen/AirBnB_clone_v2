33d89ccacc1d2091b68b170753212e5ce142ecb2
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-24 17:27:21 +0300

    fix

--- models/state.py
+++ models/state.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
-from datetime import datetime
-from models.city import City
+""" holds class State"""
 import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ Rep. a state table in mysql db and inherits from SQLAlchemy
-    Base class"""
-    __tablename__ = 'states'
-    #for task 8 checker
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    ###
-    name = Column(String(128), nullable=False)
-    if (getenv('HBNB_TYPE_STORAGE') != 'db'):
+    """Representation of state """
+    #from airbnb v3
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE')!= "db":
         @property
         def cities(self):
-            """cities getter in case file storage"""
+            """getter for list of city instances related to the state"""
             city_list = []
-            for city in (models.storage.all(City).values()):
-                if (self.id == city.state_id):
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
                     city_list.append(city)
             return city_list
-    else:
-        cities = relationship('City', backref='state', cascade='delete')
